254cf59af4c2e08ef69b64274c66fa2adb29635a
Evan Machnic <emachnic@broadmac.net>
2012-10-16 11:16:07 -0400

    Increase aruba timeout

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,9 @@ ENV['PATH'] = "#{File.expand_path(File.dirname(__FILE__) + '/../../bin')}#{File:
 LIB_DIR = File.join(File.expand_path(File.dirname(__FILE__)),'..','..','lib')
 
 Before do
+  # Increase timeout for Aruba
+  @aruba_timeout_seconds = 20
+
   # Using "announce" causes massive warnings on 1.9.2
   @puts = true
   @original_rubylib = ENV['RUBYLIB']
